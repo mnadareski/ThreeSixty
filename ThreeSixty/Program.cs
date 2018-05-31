e30f3b2b4c31cf8b11b13ba639e60cc42de03ee8
Matt Nadareski <mnadareski@outlook.com>
2018-05-30 23:17:20 -0700

    Technically support 1.44MB images

--- ThreeSixty/Program.cs
+++ ThreeSixty/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ThreeSixty
 {
-	class Program
+	public class Program
 	{
-		private const int _sectorSize = 0x2400;
+		private const int _fiveTwoFiveFortyTrackFileSize = 360 * 1024;
+		private const int _fiveTwoFiveEightyTrackFileSize = 720 * 1024;
+		private const int _fiveTwoFiveSectorSize = 0x2400;
+		private const string _fiveTwoFiveExtension = ".360";
+
+		private const int _threeFiveFortyTrackFileSize = 720 * 1024;
+		private const int _threeFiveEightyTrackFileSize = 1440 * 1024;
+		private const int _threeFiveSectorSize = 0x2400; // TODO: Verify this
+		private const string _threeFiveExtension = ".720";
 
 		static void Main(string[] args)
 		{
@@ -23,41 +28,62 @@ namespace ThreeSixty
 
 		private static void Convert(string path)
 		{
+			// Ensure the file exists
 			if (!File.Exists(path))
 			{
 				Console.WriteLine("File '{0}' did not exist", path);
 				return;
 			}
 
+			// Check that the file size matches an 80-track image
 			FileInfo fi = new FileInfo(path);
-			if (fi.Length != 720 * 1024)
+			if (fi.Length != _fiveTwoFiveEightyTrackFileSize
+				&& fi.Length != _threeFiveEightyTrackFileSize)
 			{
-				Console.WriteLine("File '{0}' was not 720k", path);
+				Console.WriteLine("File '{0}' was not a valid 80-track file size", path);
 				return;
 			}
 
+			// Get the output path
 			path = Path.GetFullPath(path);
-			string newpath = path + ".360";
+			string newpath = path;
+			int sectorSize = 0;
+			switch (fi.Length)
+			{
+				case _fiveTwoFiveEightyTrackFileSize:
+					newpath += _fiveTwoFiveExtension;
+					sectorSize = _fiveTwoFiveSectorSize;
+					break;
+				case _threeFiveEightyTrackFileSize:
+					newpath += _threeFiveExtension;
+					sectorSize = _threeFiveSectorSize;
+					break;
+				default:
+					Console.WriteLine("File '{0}' was not a valid 80-track file size");
+					return;
+			}
 
+			// Check to see if the image is truely the incorrect size (second sector should be null)
 			using (BinaryReader br = new BinaryReader(File.OpenRead(path)))
 			{
-				br.ReadBytes(_sectorSize);
-				byte[] buffer = br.ReadBytes(_sectorSize);
+				br.ReadBytes(sectorSize);
+				byte[] buffer = br.ReadBytes(sectorSize);
 
 				if (buffer.Any(b => b != 0x00))
 				{
-					Console.WriteLine("File '{0}' was a valid 720k image", path);
+					Console.WriteLine("File '{0}' was a valid 80-track image", path);
 					return;
 				}
 			}
-
+			
+			// Create the output file
 			using (BinaryReader br = new BinaryReader(File.OpenRead(path)))
 			using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(newpath)))
 			{
 				bool even = true;
 				while (br.BaseStream.Position < br.BaseStream.Length)
 				{
-					byte[] buffer = br.ReadBytes(_sectorSize);
+					byte[] buffer = br.ReadBytes(sectorSize);
 					if (even)
 					{
 						bw.Write(buffer);
@@ -67,7 +93,7 @@ namespace ThreeSixty
 				}
 			}
 
-			Console.WriteLine("File '{0}' was converted to a 360k image", path);
+			Console.WriteLine("File '{0}' was converted to a 40-track image", path);
 		}
 	}
 }
