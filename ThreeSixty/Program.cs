5e78514c98efee821e33816452b3d2df5a2e5473
Matt Nadareski <mnadareski@outlook.com>
2018-05-30 20:47:25 -0700

    Initial commit

--- ThreeSixty/Program.cs
+++ ThreeSixty/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace ThreeSixty
 	{
 		static void Main(string[] args)
 		{
+			foreach (string arg in args)
+			{
+				Convert(arg);
+			}
+		}
+
+		private static void Convert(string path)
+		{
+			if (!File.Exists(path))
+			{
+				return;
+			}
+
+			path = Path.GetFullPath(path);
+			string newpath = path + ".360";
+
+			// TODO: Currently no checking to see if this is a valid 360k floppy file
+			using (BinaryReader br = new BinaryReader(File.OpenRead(path)))
+			using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(newpath)))
+			{
+				bool even = true;
+				while (br.BaseStream.Position < br.BaseStream.Length)
+				{
+					byte[] buffer = br.ReadBytes(0x2400);
+					if (even)
+					{
+						bw.Write(buffer);
+					}
+					
+					even = !even;
+				}
+			}
 		}
 	}
 }
